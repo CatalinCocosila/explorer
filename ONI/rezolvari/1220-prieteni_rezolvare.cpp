a69296cbe784fa4942d9f65d0c6c9b67622ad290
CatalinCocosila <92211735+CatalinCocosila@users.noreply.github.com>
2025-09-04 14:53:47 +0300

    Update 1220-prieteni_rezolvare.cpp

--- ONI/rezolvari/1220-prieteni_rezolvare.cpp
+++ ONI/rezolvari/1220-prieteni_rezolvare.cpp
@@ -1,56 +1,58 @@
-#include<bits/stdc++.h>
- 
+#include <fstream>
+#include <iostream>
 using namespace std;
-
-
+ifstream fin("prieteni.in");
+ofstream fout("prieteni.out");
+int sumacif(int nr){
+    int s = 0 ; 
+    while(nr){
+        s +=( nr % 10);
+        nr = nr / 10;
+    }
+    return s;
+}
+int cmmdc(int a, int b){
+         while(b){
+           int r = a %b ;
+           a = b;
+           b = r ;
+       }
+           return a;
+}
 int main()
 {
-	
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	ifstream cin("prieteni.in");
-	ofstream cout("prieteni.out");
-	
-	vector<int> v;
-	
-	vector<int> pr(70002);
-	for(int i = 2; i <= 70000; i++)
-		if(pr[i] == 0)	
-		{
-			v.push_back(i);
-			for(int j = i+i; j <= 70000; j += i)
-				pr[j] = 1;
-		}
-	
-	vector<int> ans;
-	int n;
-	cin >> n;
-	vector<int> val(n+1);
-	
-	for(int i = 1; i <= n; i++)
-		cin >> val[i], pr[val[i]] = 1;
-	
-	for(int i = 1; i <= n; i++)
-	{
-		int xx = val[i], sc = 0;
-		while(xx)
-		{
-			sc += xx%10;
-			xx /= 10;
-		}
-		if(__gcd(sc, val[i]) == 1)
-		{
-			for(int j = sc; j <= val[i]; j++)
-				if(pr[j] == 0)
-					ans.push_back(j), pr[j] = 1;
-		}
-		ans.push_back(val[i]);
-	}
-	
-	cout << ans.size() << '\n';
-	
-	for(auto x : ans)
-		cout << x << " ";
-	return 0;
+    bool prim[65535]={};
+    int j,d,n ,v[100000]={},s,vf[100000]={},i1=0;
+    prim[0]=prim[1]=1;
+    for(j=2;j<65535;j++){
+        if(prim[j]==0){
+        for(d=j+j;d<65535;d+=j){
+            prim[d] = 1;
+        }
+        }
+    }
+    fin>>n;
+    for(int i = 1 ; i <= n ; i++){
+        fin>>v[i];
+        prim[v[i]] = 1; // consider ca v[i] nu e prim pentru ca mai jos o sa pun elemente in vectorul nou format si nu trebuie pus si v[i] 
+    }
+   for(int i = 1 ; i <= n ; i++){
+       s = sumacif(v[i]);
+       if(cmmdc(v[i],s)==1){
+           for(j = s ; j <= v[i]; j++){
+               if(prim[j]==0){
+                   i1++;
+                   vf[i1] = j;
+                   prim[j]=1;
+               }
+           }
+       }
+       i1++;
+       vf[i1]=v[i];
+   }
+   fout<<i1<<endl;
+   for(int i = 1; i <= i1 ; i++){
+       fout<<vf[i]<<" ";
+   }
+    return 0;
 }
